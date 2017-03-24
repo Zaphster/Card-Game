39685834d45d4bde5571f69f41fab7292200afb1
Zaphster <Zaphster@users.noreply.github.com>
2017-03-23 22:14:22 -0500

    Create MinionStatus.java

---
+++ MinionStatus.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventbase;
+
+/**
+ *
+ * @author Cameron
+ */
+public enum MinionStatus {
+    SUMMONED,
+    MOVING,
+    ATTACKING,
+    DEFENDING,
+    DEAD,
+    HEALING,
+    DEALINGDAMAGE,
+    RECEIVINGDAMAGE,
+    BEINGSACRIFICED,
+    BEINGIMPRINTED,
+}
